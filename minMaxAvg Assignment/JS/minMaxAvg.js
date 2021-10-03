0a02d52030f36d628b957a398dcbbb3b91d35deb
Kah Fei CHIN <octoberchin@hotmail.com>
2021-10-03 22:57:51 +0800

    minMaxAvg Assignment

---
+++ minMaxAvg Assignment/JS/minMaxAvg.js
@@ -0,0 +1,15 @@
+const arr = getMinMaxAvg(1, 2)
+
+function getMinMaxAvg(a, b){
+ 
+  let min = Math.min(a, b)
+  let Max = Math.max(a, b)
+  let Avg = (a + b) / getMinMaxAvg.length
+  
+var minMaxAvg = ['Minimum = ' + min, ' Maximum = ' + Max, ' Average = ' + Avg]
+console.log(minMaxAvg)
+
+let elresult = document.getElementById('result')
+elresult.innerHTML = 'The result is ' + minMaxAvg
+
+} 
